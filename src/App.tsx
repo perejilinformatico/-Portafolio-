33b35a1d02b0e623e4079d7ea38647a6526c21bc
Santiago <perejilinformatico@gmail.com>
2026-03-03 23:13:30 +0000

    Cambios sin terminar

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,18 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='text-center flex justify-center items-center h-40'>hola</h1>
+      <header className='flex justify-between items-center text-center'>
+        <h1 className='text-3xl'>Santiago</h1>
+        <div>
+          <img src='/burger128.png' alt='Barra_de_datos' className='w-10 m-1' onClick={Click}/>
+        </div>
+      </header>
     </>
   )
+}
+
+function Click() {
+  
 }
 
 export default App
