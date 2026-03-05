e11c7557d676b7b9f51e3c7857180159d5cfb9d9
Santiago <perejilinformatico@gmail.com>
2026-03-05 22:00:06 +0000

    Nueva rama, reparaciones

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,27 @@ import { useState } from 'react';
 
 
 function App() {
+  let i = '/burger128.png';
   const [active, setActive] = useState(false);
-  
+
   function Click() {
      console.log("Menu sobre mi abierto");
      setActive(true);
+     i = '/close'
+  }
+  function handClick() {
+      setActive(false);
+  }
+  function Card() {
+     return (
+        <>
+        </>
+     )
   }
   return (
     <>
     <div>
-          <div className='bg-gray-300 flex justify-center items-center flex-col text-center gap-3' id='menu_desplegable' style={{ display: active ? 'flex' : 'none' }}>
-          <div>
-            <img src='/close128.png' alt='salir_icono' className='my-8'/>
-          </div>
+          <div className='bg-gray-300 flex justify-center items-center flex-col text-center gap-3' id='menu_desplegable' style={{ display: active ? 'flex' : 'none' }} onClick={handClick}>
           <h1 className='text-20'>¡Hola!</h1>
           <h2 className=''>¡Soy santiago, un programador full stack! :D</h2>
           <p className=''>Perdon por el vago portafolio pero este portafolio, esta hecho para mostrar proyectos <br /> 
@@ -24,11 +32,20 @@ function App() {
           <p>La verdad es simple por q encontre un diseño simple, hice el mio y asi quedo 👀</p>
       </div>
       <header className='flex justify-between items-center text-center'>
+<<<<<<< HEAD
         <h1 className='text-3xl font-bold relative bottom-1 my-3 ml-1' id="santiago__h1">Santiago</h1>
+=======
+        <h1 className='text-3xl font-bold relative bottom-1 my-3.5' id="santiago__h1">Santiago</h1>
+>>>>>>> 93aaf5d (Nuevos cambios)
         <div>
-          <img src='/burger128.png' alt='Barra_de_datos' className='w-10 m-1 my-3' onClick={Click}/>
+          <img src={i} alt='Barra_de_datos' className='w-10 m-1 my-3' onClick={Click}/>
         </div>
       </header>
+      <div>
+        <div>
+          <Card/>
+        </div>
+      </div>
     </div>
     </>
   )
